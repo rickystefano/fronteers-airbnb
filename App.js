03bf7aa68198080ca76bf7a52204f88ddaf6d2db
Ricky Simadinata <528101@student.glu.nl>
2018-11-23 10:21:46 +0100

    3, 4: More option to BottomNavigator; added second component and Route

--- App.js
+++ App.js
@@ -1,22 +1,53 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { createBottomTabNavigator } from "react-navigation";
+import { FontAwesome } from "@expo/vector-icons";
 
-class App extends React.Component {
+class Home extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open developer menu.</Text>
+        <Text>Home</Text>
       </View>
     );
   }
 }
 
+class Profile extends React.Component {
+  render() {
+    return (
+      <View style={style.container}>
+        <Text>Profiel</Text>
+      </View>
+    )
+  }
+}
+
 export default createBottomTabNavigator(
   {
-    Home: App //Bind App to route Home
+    Home, //ES6+
+    Profile
+  },
+  {
+    navigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        switch (routeName) {
+          case 'Home':
+            iconName = 'search';
+            break;
+          case 'Profile':
+            iconName = 'user-o'
+            break;
+        }
+        return <FontAwesome name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: '#fd5c63',
+      inactiveTintColor: 'gray',
+    }
   }
 );
 
