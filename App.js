7cbe1795c72d2cdf30340089c09fb9bf7c413deb
Ricky Simadinata <528101@student.glu.nl>
2018-12-07 09:45:09 +0100

    firebase added, splash screen and AppLoading added

--- App.js
+++ App.js
@@ -3,11 +3,21 @@ import { View, Text } from 'react-native';
 import { createBottomTabNavigator } from "react-navigation";
 import { FontAwesome } from "@expo/vector-icons";
 import EStyleSheet from "react-native-extended-stylesheet";
-
+import * as firebase from 'firebase';
 import Home from "./Screens/Home";
 import Profile from "./Screens/Profile";
 
 EStyleSheet.build();
+
+//Init firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyAgOcNvVc0_Y-EPJh4xEmwYvSoTv3GTI4k',
+  authDomain: 'fronteers-8bd8c.firebaseapp.com',
+  databaseURL: 'https://fronteers-8bd8c.firebaseio.com',
+  storageBucket: 'fronteers-8bd8c.appspot.com'
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default createBottomTabNavigator(
   {
--- Screens/Home.js
+++ Screens/Home.js
@@ -4,27 +4,29 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import { createStackNavigator } from 'react-navigation';
 import { Categories } from '../Components/Categories'
 import { Listings } from '../Components/Listings';
-import { Listing } from './Listing'
+import { Listing } from './Listing';
+import * as firebase from 'firebase';
+import { AppLoading } from "expo";
 
 class HomeScreen extends React.Component {
 
-  render() {
-    const categories = [
-      { "key": "1", "title": "Accomodations", "image": "http://a0.muscache.com/im/pictures/8b7519ec-2c82-4c09-8233-fd4d2715bbf9.jpg?aki_policy=small" },
-      { "key": "2", "title": "Experiences", "image": "http://a0.muscache.com/im/pictures/cb8b3101-d419-4c17-8e2f-4989b39b98c3.jpg?aki_policy=small" },
-      { "key": "3", "title": "Restaurants", "image": "http://a0.muscache.com/im/pictures/da2d8e97-90b7-409f-94ac-5ab0327c289b.jpg?aki_policy=small" }
-    ];
+  componentDidMount() {
+    this.getData('categories');
+    this.getData('listings');
+  }
 
-    const listings = [
-      { "key": "1", "title": "I Sette Coni - Trullo Edera", "image": "http://a0.muscache.com/im/pictures/15273358/d7329e9a_original.jpg?aki_policy=small" },
-      { "key": "2", "title": "Luxury Gold Coast Hinterland Villa", "image": "http://a0.muscache.com/im/pictures/7787384/03380852_original.jpg?aki_policy=small" },
-      { "key": "3", "title": "Luxurious stone villa in Crete", "image": "http://a0.muscache.com/im/pictures/17588525/94250ea3_original.jpg?aki_policy=small" },
-      { "key": "4", "title": "Best location design quiet AC free wifi citycenter", "image": "http://a0.muscache.com/im/pictures/6061582/a643208f_original.jpg?aki_policy=small" },
-      { "key": "5", "title": "I Sette Coni - Trullo Edera", "image": "http://a0.muscache.com/im/pictures/15273358/d7329e9a_original.jpg?aki_policy=small" },
-      { "key": "6", "title": "Luxury Gold Coast Hinterland Villa", "image": "http://a0.muscache.com/im/pictures/7787384/03380852_original.jpg?aki_policy=small" },
-      { "key": "7", "title": "Luxurious stone villa in Crete", "image": "http://a0.muscache.com/im/pictures/17588525/94250ea3_original.jpg?aki_policy=small" },
-      { "key": "8", "title": "Best location design quiet AC free wifi citycenter", "image": "http://a0.muscache.com/im/pictures/6061582/a643208f_original.jpg?aki_policy=small" }
-    ];
+  getData = (ref) => {
+    firebase.database().ref(ref).once('value', (snapshot) => {
+      this.setState({ [ref]: snapshot.val() })
+    })
+  }
+
+  render() {
+    const { categories, listings } = (this.state) ? this.state : {};
+
+    if (!categories || !listings) {
+      return <AppLoading />
+    }
 
     return (
       <View style={styles.container}>
